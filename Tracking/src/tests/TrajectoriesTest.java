ae015aed1c4f20a0438e77d00c8acde0cdccdfd1
pedrogil1919 <pedrogil1919@gmail.com>
2015-03-09 12:27:27 +0100

    Add XML support for program settings

--- Tracking/src/tests/TrajectoriesTest.java
+++ Tracking/src/tests/TrajectoriesTest.java
@@ -1,20 +1,40 @@
 package tests;
 
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
 import org.opencv.core.*;
 import org.opencv.highgui.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import trajectories.Trajectories;
 
 public class TrajectoriesTest {
 
-	public static void main(String args[]) throws ParserConfigurationException, TransformerException{
+	public static void main(String args[]) throws ParserConfigurationException, TransformerException, SAXException, IOException{
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		VideoCapture video = new VideoCapture();
-		video.open("/home/pedro/Videos/GTT/gtt-v1.avi");
+		
+		//Open settings XML
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse("settings.xml");
+		document.getDocumentElement().normalize();
+		
+		NodeList settings = document.getElementsByTagName("video");
+		Element setVideo = (Element) settings.item(0);
+		String path = setVideo.getAttribute("path");
+		String file = setVideo.getAttribute("file");
+		
+		video.open(path+file);
 		
 		if (!video.isOpened()) {
 			System.out.println("Error. Can not be open video");
